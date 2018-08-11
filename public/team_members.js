bc77f166d0c7676f350f293e9412bf12fbec0619
parimatrix <parikansh.ahluwalia@gmail.com>
2018-08-11 12:33:58 +0530

    Update Team Members

--- public/team_members.js
+++ public/team_members.js
@@ -1,15 +1,14 @@
 $(function() {
-    var details = ["Prachi Manchanda", "prachimanchanda", "Director",
-                   "Sarthak Agarwal", "sarthakagarwal", "Director, Operations",
-                   "Karanjot Singh", "karanjotsingh", "Director, Technology",
-                   "Mansimar Kaur", "mansimar", "Senior Board Member",
-                   "Kamakshi Suri", "kamakshisuri", "Senior Board Member",
-                "Ayush Garg", "ayushgarg2018", "Senior Board Member",
-                "Sagorika Das", "sagorikadas", "Senior Board Member",
-                "Shivli Aggarwal", "shivliagrawal", "Senior Board Member",
-                "Sijal Bhatnagar"," ","Senior Board Member",
-                "Vidit Bhargava", "vidit", "Senior Board Member",
-                "Deepika Naryani", "deepikanaryani", "Senior Board Member"
+    var details = ["Paavini Nanda", "paavininanda", "Director, Technology",
+                   "Mohammad Salik", "salik", "Director, Technology",
+                   "Tanya Gupta", "tanyagupta", "Director, Operations",
+                   "Sameer Garg", "sameergarg", "Director, Finance",
+                   "Karanjot Singh", "karanjotsingh", "Director, Web",
+                   "Aabhas Gupta", "aabhasgupta", "Director, Public Relations",
+                "Ashish Yadav", "unknown-male", "Senior Coordinator",
+                "Ashwani Singh", "unknown-male", "Senior Coordinator",
+                "Jayank Mayukh", "jayankmayukh", "Senior Coordinator",
+                "Shubham Priyadarshi", "shubhampriyadarshi", "Senior Coordinator"
                 ];
     
     var cover = $('#cover');
@@ -24,51 +23,58 @@ $(function() {
                         '</div>');
         }
     cover.append('<br><br><br><br>');
+    
     cover.append('<div class="person">' +
             '<img src="https://avatars0.githubusercontent.com/u/24352485?s=400&v=4">'+
             '<p>Rohit Suri<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
-    /*
-    cover.append('<div class="person">' +
-            '<img src="https://avatars1.githubusercontent.com/u/25114873?s=460&v=4">'+
-            '<p>Parikansh Ahluwalia<br>'+
-            'Executive Member</p>'+
-        '</div>');
-    */
+    
     cover.append('<div class="person">' +
             '<img src="https://avatars3.githubusercontent.com/u/26252118?s=400&v=4">'+
             '<p>Anshul Mittal<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
+    
+    cover.append('<div class="person">' +
+            '<img src="https://avatars1.githubusercontent.com/u/25114873?s=460&v=4">'+
+            '<p>Parikansh Ahluwalia<br>'+
+            'Board Member</p>'+
+        '</div>');
+    
     cover.append('<div class="person">' +
             '<img src="team_images/TanviDadu.jpg">'+
             '<p>Tanvi Dadu<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
     cover.append('<div class="person">' +
             '<img src="team_images/rahmeenhabib.jpg">'+
             '<p>Rahmeen Habib<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
     cover.append('<div class="person">' +
-            '<img src="team_images/mayurvaid.jpg">'+
+            '<img src="team_images/mayurvaid.JPG">'+
             '<p>Mayur Vaid<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
     cover.append('<div class="person">' +
             '<img src="team_images/jaikirat.jpg">'+
             '<p>Jaikirat Singh<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
     cover.append('<div class="person">' +
             '<img src="team_images/ishanarora.jpg">'+
             '<p>Ishan Arora<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
         '</div>');
     cover.append('<div class="person">' +
             '<img src="team_images/abhishekvanjani.jpg">'+
             '<p>Abhishek Vanjani<br>'+
-            'Executive Member</p>'+
+            'Board Member</p>'+
+        '</div>');
+    cover.append('<div class="person">' +
+            '<img src="team_images/awesid.jpg">'+
+            '<p>Abhishek Vanjani<br>'+
+            'Board Member</p>'+
         '</div>');
 });
